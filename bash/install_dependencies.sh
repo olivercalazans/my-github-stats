0b8a9534087ae1be03bc2b9399381151622340b3
Oliver Calazans <oliver.calazans@gmail.com>
2026-08-23 17:42:51 -0300

    create and init venv via bash script

--- bash/install_dependencies.sh
+++ bash/install_dependencies.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install dependencies
 apt update && apt install -y python3 python3-pip
+
+# Create virtual enviroment
+python3 -m venv venv
+
+# Init virtual enviroment
+source ./venv/bin/activate
+
+# Install python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
